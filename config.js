96cc8118b987919b115c943bb4757d469d39b615
Aman Tandon <iaman221b@gmail.com>
2020-10-12 23:40:26 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "eb9eb319fcba63268f8b2a20a0065b58"
+    }
+}
+
+module.exports = constants;
